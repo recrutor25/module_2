b170d74c43a3f1f7b0f19dbec741163ecce61697
recrutor25 <sommersi@yandex.ru>
2024-09-29 08:51:32 +0200

    28.09

--- Module_2_2.py
+++ Module_2_2.py
@@ -4,9 +4,14 @@
 Created on 20 09 2024
 @author: igor
 """
+first = int(input('введите первое число:   '))
+second = int(input('введите второе число:   '))
+third = int(input('введите третье число:   '))
 
-print('Hi, PyCharm')
-x = 43
-y = 32
-print(x * y)
-print("End line")
+if first == second and first == third and second == third:
+    print('3') # если равны все числа
+elif first == second or first == third or second == third:
+    print('2') # если равны любые два числа
+else:
+    print('0') # иначе числа не равны
+
