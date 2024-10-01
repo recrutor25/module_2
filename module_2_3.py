31544568d10c0d97a14d5e57e86866379a503d19
recrutor25 <sommersi@yandex.ru>
2024-10-01 21:38:28 +0200

    28.09

--- module_2_3.py
+++ module_2_3.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on 20 09 2024
+Created on 30 09 2024
 @author: igor
 """
-s = 'repka'
-l = list(s)
-print(l)
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+n = 0
+while n < len(my_list):
+    if my_list[n] > 0:
+        print(my_list[n])
+    elif my_list[n] == 0:
+        n += 1
+        continue
+    else:
+        break
+    n += 1
+
---
+++ modulle_2_4.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 30 09 2024
+@author: igor
+"""
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+#n = input("n=")
+primes = []
+not_primes = []
+is_prime = True
+# флаг позволяет пропустить 2 т.к. range(2,2) пробросит вложеный цикл
+for i in numbers[1:]: # перебор списка начиная с двойки
+    # перебор делителей
+    for j in range(2, i):
+        if i % j == 0 and i > 2:
+            is_prime = False
+            break
+        else: is_prime = True
+
+    if is_prime :
+        primes.append(i)
+    else:
+        not_primes.append(i)
+print(primes)
+print(not_primes)
+
