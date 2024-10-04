f74fb5f2738687097d3d47fb25a9d48f40f849ed
recrutor25 <sommersi@yandex.ru>
2024-10-04 21:47:45 +0200

    28.09

--- module_2_5.py
+++ module_2_5.py
@@ -6,12 +6,18 @@ Created on 02 10 2024
 """
 def get_matrix(n, m, value):
     matrix = []
-    for i in range(n):
-        matrix.append([])
-        for j in range(m):
-            matrix[i].append(value)  # Пишем в список значение
-    print(matrix)
+    if n and m > 0 :
+        for i in range(n):
+            matrix.append([])
+            for j in range(m):
+                matrix[i].append(value)  # Пишем в список значение
+        print(matrix)
+    else:
+        print('Строка или столбец не могут быть равны нолю','\n', \
+            'повторите ввод')
+
+
 result1 = get_matrix(2, 2,10)
-result2 = get_matrix(3, 5, 42)
+result2 = get_matrix(0, 5, 42)
 result3 = get_matrix(4, 2, 13)
 
